9acefc69cba1d11e81f67fb53e6490fe29bb14dc
aj-0 <ajay360x@gmail.com>
2026-03-21 11:52:55 +0530

    Delete filterbot directory

--- filterbot/database/__init__.py
+++
@@ -1,6 +0,0 @@
-from .db import (
-    ensure_indexes, save_file, search_files, delete_all_files, count_files,
-    add_user, total_users, get_all_users,
-    add_group, total_groups,
-    get_group_settings, update_group_setting
-)
--- filterbot/web/__init__.py
+++
@@ -1,1 +0,0 @@
-from .server import web_server
